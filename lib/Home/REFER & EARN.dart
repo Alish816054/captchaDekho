534318f1ac98c40a5d5b3629f9182ca4608896a5
Alish816054 <alishpatel9599@gmail.com>
2025-04-22 17:00:21 +0530

    Comit and change

--- lib/Home/REFER & EARN.dart
+++ lib/Home/REFER & EARN.dart
@@ -12,6 +12,77 @@ class REFERANDEARN extends StatelessWidget {
         title: ("REFER & EARN"),
         backgroundColor: Color(0xFF8664E7),
       ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(28.0),
+            child: Container(
+              height: 180,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.topRight,
+                  colors: [Color(0XFF3F857C), Color(0XFF384296)],
+                ),
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40, left: 30),
+                        child: Text(
+                          "Refer now & earn",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 22,
+                            fontFamily: "Volkhov",
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, top: 10),
+                        child: Row(
+                          children: [
+                            Text(
+                              "up to",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                fontFamily: "Volkhov",
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              " ₹500",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                fontFamily: "Volkhov",
+                                color: Colors.yellow,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Row(children: []),
+          // Row(children: []),
+          // Row(children: []),
+          // Row(children: []),
+          // Row(children: []),
+          // Row(children: []),
+          // Row(children: []),
+        ],
+      ),
     );
   }
 }
